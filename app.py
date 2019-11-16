2e5d46c2ba529b7b20e0621fb3d7c1ee707b6175
Kerim Kochekov <50640842+KerimKochekov@users.noreply.github.com>
2019-11-16 14:42:58 +0300

    Add files via upload

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from tmp import ODE_GRAPH
 root = tk.Tk()
 def derivative(y,x): 
     x = float("{0:.5f}".format(x))
-    return -(1/3*y*y)-(2/(3*x*x))
+    return -((y*y)/3)-(2/(3*x*x))
+    
 gui = ODE_GRAPH(root,derivative)
 root.mainloop()
--- draw.py
+++ draw.py
@@ -9,7 +9,12 @@ def Graph(f1,x0,X,y0,derivative):
     xf = normalize(x0, X, 0.001)
     yf = odeint(derivative,y0,xf)
     f1.add_subplot(111).plot(xf,yf,color='black')
-
+def Error(a,b):
+    er = []
+    for i in range(len(a)):
+        er.append(abs(a[i]-b[i]))
+    return er
+    
 def Euler(f1,f2,f3,x0,X,y0,derivative,n,N):
     h,yy = (X-x0)/N,y0
     xf,ey = normalize(x0,X,h),[]
@@ -19,23 +24,19 @@ def Euler(f1,f2,f3,x0,X,y0,derivative,n,N):
         yy = yy+h*derivative(yy,xx)
         yy = float("{0:.5f}".format(yy))
     f1.add_subplot(111).plot(xf,ey,color="red")
-
-    er = []
-    for i in range(len(yf)):
-        er.append(abs(yf[i]-ey[i]))
-    f2.add_subplot(111).plot(xf,er,color="red")
+    f2.add_subplot(111).plot(xf,Error(yf,ey),color="red")
 
     xn,yn = normalize(n,N,1),[]
     for x in xn:
-        h,yy,mx = (X-x0)/x,y0,0
+        h,yy = (X-x0)/x,y0
         xf = normalize(x0,X,h)
         yf = odeint(derivative,y0,xf)
-        for i in range(len(xf)):
-            yy = yy+h*derivative(yy,xf[i])
+        ey = []
+        for xx in xf:
+            ey.append(yy)
+            yy = yy+h*derivative(yy,xx)
             yy = float("{0:.5f}".format(yy))
-            if abs(yy-yf[i]) > mx:
-                mx = abs(yy-yf[i])
-        yn.append(mx)
+        yn.append(max(Error(yf,ey)))
     f3.add_subplot(111).plot(xn,yn,color="red")
 
 
@@ -50,25 +51,21 @@ def Improved_Euler(f1,f2,f3,x0,X,y0,derivative,n,N):
         yy = yy+h*(k1+k2)/2
         yy = float("{0:.5f}".format(yy))
     f1.add_subplot(111).plot(xf,ey,color="blue")
-
-    er = []
-    for i in range(len(yf)):
-        er.append(abs(yf[i]-ey[i]))
-    f2.add_subplot(111).plot(xf,er,color="blue")
+    f2.add_subplot(111).plot(xf,Error(yf,ey),color="blue")
     
     xn,yn = normalize(n,N,1),[]
     for x in xn:
-        h,yy,mx = (X-x0)/x,y0,0
+        h,yy = (X-x0)/x,y0
         xf = normalize(x0,X,h)
         yf = odeint(derivative,y0,xf)
-        for i in range(len(xf)):
-            k1 = derivative(yy,xf[i])
-            k2 = derivative(yy+h*k1,xf[i]+h)
+        ey = []
+        for xx in xf:
+            ey.append(yy)
+            k1 = derivative(yy,xx)
+            k2 = derivative(yy+h*k1,xx+h)
             yy = yy+h*(k1+k2)/2
             yy = float("{0:.5f}".format(yy))
-            if abs(yy-yf[i]) > mx:
-                mx = abs(yy-yf[i])
-        yn.append(mx)
+        yn.append(max(Error(yf,ey)))
     f3.add_subplot(111).plot(xn,yn,color="blue")
 
 def Runge(f1,f2,f3,x0,X,y0,derivative,n,N):
@@ -81,27 +78,24 @@ def Runge(f1,f2,f3,x0,X,y0,derivative,n,N):
         k2 = derivative(yy+h/2*k1,xx+h/2)
         k3 = derivative(yy+h/2*k2,xx+h/2)
         k4 = derivative(yy+h*k3,xx+h)
-        yy = yy+h*(k1+k2+k3+k4)/6
+        yy = yy+h*(k1+2*k2+2*k3+k4)/6
         yy = float("{0:.5f}".format(yy))
     f1.add_subplot(111).plot(xf,ey,color="green")
-
-    er = []
-    for i in range(len(yf)):
-        er.append(abs(yf[i]-ey[i]))
-    f2.add_subplot(111).plot(xf,er,color="green")
+    f2.add_subplot(111).plot(xf,Error(yf,ey),color="green")
 
     xn,yn = normalize(n,N,1),[]
     for x in xn:
-        h,yy,mx = (X-x0)/x,y0,0
+        h,yy = (X-x0)/x,y0
         xf = normalize(x0,X,h)
         yf = odeint(derivative,y0,xf)
-        for i in range(len(xf)):
-            k1 = derivative(yy,xf[i])
-            k2 = derivative(yy+h/2*k1,xf[i]+h/2)
-            k3 = derivative(yy+h/2*k2,xf[i]+h/2)
-            k4 = derivative(yy+h*k3,xf[i]+h)
+        ey = []
+        for xx in xf:
+            ey.append(yy)
+            k1 = derivative(yy,xx)
+            k2 = derivative(yy+h/2*k1,xx+h/2)
+            k3 = derivative(yy+h/2*k2,xx+h/2)
+            k4 = derivative(yy+h*k3,xx+h)
+            yy = yy+h*(k1+2*k2+2*k3+k4)/6
             yy = float("{0:.5f}".format(yy))
-            if abs(yy-yf[i]) > mx:
-                mx = abs(yy-yf[i])
-        yn.append(mx)
+        yn.append(max(Error(yf,ey)))
     f3.add_subplot(111).plot(xn,yn,color="green")
